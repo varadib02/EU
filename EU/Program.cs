44303e668b517dce418d0a28e186d6ecaf561c1c
varadib02 <barkabas02@gmail.com>
2022-01-18 10:29:27 +0100

    beolvasas

--- EU/Program.cs
+++ EU/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 namespace EU
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            List<csatlakozas> orszagok = new List<csatlakozas>();
+            foreach (var sor in File.ReadAllLines("EUcsatlakozas.txt").Skip(1))
+            {
+                orszagok.Add(new csatlakozas(sor));
+            }
         }
     }
 }
---
+++ EU/csatlakozas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU
+{
+    class csatlakozas
+    {
+        public string orsz { get; set; }
+        public DateTime datum { get; set; }
+
+        public csatlakozas(string sor)
+        {
+            string []db = sor.Split(';');
+            orsz = db[0];
+            datum = DateTime.Parse(db[1]);
+        }
+    }
+}
