f8bda80577016bc51f08116e2a9dc42089627fde
varadib02 <barkabas02@gmail.com>
2022-01-18 11:15:51 +0100

    8.f

--- EU/Program.cs
+++ EU/Program.cs
@@ -11,17 +11,15 @@ namespace EU
         static void Main(string[] args)
         {
             List<csatlakozas> orszagok = new List<csatlakozas>();
-            foreach (var sor in File.ReadAllLines("EUcsatlakozas.txt"))
-            {
-                orszagok.Add(new csatlakozas(sor));
-            }
+            foreach (var sor in File.ReadAllLines("EUcsatlakozas.txt"))orszagok.Add(new csatlakozas(sor));
             Console.WriteLine($"3.feladat: Az Eu tagállamainak száma: {orszagok.Count} db");
             Console.WriteLine($"4.feladat: 2007-ben {orszagok.Count(x=>x.datum.Year==2007)} ország csatlakozott.");
-            csatlakozas hun = orszagok
-                .Where(x => x.orsz == "Magyarország").First();
+            csatlakozas hun = orszagok.Where(x => x.orsz == "Magyarország").First();
             Console.WriteLine($"5.feladat: Magyarország csatlakozásának a dátuma: {hun.datum:d}");
             Console.WriteLine($"6.feladat: Májusban {(orszagok.Any(x=>x.datum.Month==05)?"volt":"nem volt")} csatlakozás!");
-            Console.WriteLine($"7.feladat: Statisztika");
+            csatlakozas utolso = orszagok.OrderBy(x => x.datum.Year).Last();
+            Console.WriteLine($"7.feladat: Legutoljára csatlakozott ország: {utolso.orsz}");
+            Console.WriteLine($"8.feladat: Statisztika");
             orszagok.GroupBy(x => x.datum.Year).Select(x => new { d = x.Key, c = x.Count() }).ToList().ForEach(x => Console.WriteLine($"\t{x.d} - {x.c}"));
         }
     }
